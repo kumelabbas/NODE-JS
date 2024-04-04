5179b70bbd94e9804b331758999a976b9f58892d
kumelabbas <165946241+kumelabbas@users.noreply.github.com>
2024-04-04 11:24:44 +0500

    Add files via upload

---
+++ kumel.js
@@ -0,0 +1,18 @@
+//const fs = require("fs");
+//fs.writeFileSync("kumel.txt","Its Okay to not be Okay");
+
+const readline = require("readline");
+
+//const rl = readline.createInterface({
+  //  input: process.stdin,
+    //output: process.stdout
+//});
+
+//rl.question("WHATS IS MY NAME ",function(answer){
+  //  console.log("YOUR NAME IS : "+answer)
+//})
+
+//const prompt = require("prompt-sync")();
+//const input = prompt("WHAT IS YOUR NAME");
+
+//console.log(input)
